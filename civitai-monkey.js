658eb027c6d3f3cdec27e41664a9ba54321e6aca
byte.nerd.alpha <byte.nerd.alpha@gmail.com>
2024-03-14 01:09:57 +0530

    Add CivitAI resource downloader script
    Added a new JavaScript file to script civitai-monkey.js, enhancing functionality of the CivitAI web application. This script helps extract resources required to recreate a CivitAI generation and initiates their download. Additionally, it also improves user interactions by modifying buttons and tooltips.

---
+++ civitai-monkey.js
@@ -0,0 +1,128 @@
+// ==UserScript==
+// @name         CivitAI resource downloader
+// @version      0.0.1
+// @description  Extract resources required to re-create a civitai generation and downloads them
+// @author       bytenerdalpha
+// @match        https://*civitai.com/images/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=civitai.com
+// @grant        GM.xmlHttpRequest
+// @updateURL
+// ==/UserScript==
+
+// settings
+
+// Define svg path data for the icons to be used in the extension, one for JSON, one for processing one for error and one for success
+const jsonIconPath = "M20 16v-8l3 8v-8 M15 8a2 2 0 0 1 2 2v4a2 2 0 1 1 -4 0v-4a2 2 0 0 1 2 -2z M1 8h3v6.5a1.5 1.5 0 0 1 -3 0v-.5 M7 15a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-2a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1h1a1 1 0 0 1 1 1";
+const processingIconPath = "M12 6l0 -3 M16.25 7.75l2.15 -2.15 M18 12l3 0 M16.25 16.25l2.15 2.15 M12 18l0 3 M7.75 16.25l-2.15 2.15 M6 12l-3 0 M7.75 7.75l-2.15 -2.15";
+const errorIconPath = "M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0M12 9v4M12 16v.01";
+const successIconPath = "M5 12l5 5l10 -10";
+
+function postApi(resourceDownUrl, modelVersionId, resourceType, fileName, resourceNameNormalized) {
+    fetch('http://localhost:8000/civitai/v1/resource/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'   // CORS allow all
+        },
+        body: JSON.stringify({
+            url: resourceDownUrl,
+            modelVersionId: modelVersionId,
+            resourceType: resourceType,
+            name: fileName,
+            resourceNameNormalized: resourceNameNormalized
+        })
+    }).then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+const civ_down_icon = '<path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path><path d="M7 11l5 5l5 -5"></path><path d="M12 4l0 12"></path>';
+
+function replaceRunButton() {
+
+    let div = document.createElement('div');
+    // div.innerHTML = civ_down_icon;
+    // btnBar.prepend(div);
+
+
+    const iconText = document.querySelector(".mantine-Stack-root.mantine-896gbg")
+    const runButton = document.querySelector(".mantine-UnstyledButton-root.mantine-ActionIcon-root.mantine-1tbxpdw").closest("button");
+    const icon = document.querySelector(".tabler-icon.tabler-icon-x");
+    // icon.innerHTML = civ_down_icon;
+    // icon.setAttribute("d", jsonIconPath);
+    runButton.style.backgroundColor = "#343a40";
+    icon.parentElement.setAttribute("stroke", "white");
+
+
+    runButton.addEventListener("mouseover", (event) => {
+        const tooltip = runButton.previousSibling;
+        if (tooltip.classList.contains("mantine-Tooltip-tooltip")) {
+            tooltip.innerText = "Download Resources through civ-py";
+        }
+    });
+
+    const downloadURI = (uri, name) => {
+        const link = document.createElement("a");
+        link.download = name;
+        link.href = uri;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
+    runButton.addEventListener("click", async (event) => {
+        event.stopImmediatePropagation();
+        icon.setAttribute("d", processingIconPath);
+        runButton.style.backgroundColor = "#1971c2";
+        let els = document.getElementsByClassName("mantine-4xj3rk");
+
+        try {
+            for (let i = 0; i < els.length; i++) {
+                const el = els[i];
+                const resourceType = el.querySelector(".mantine-h9iq4m.mantine-Badge-inner").textContent;
+                const resourceNameFull = el.querySelector(".mantine-Text-root.mantine-p6jtwo").textContent;
+                const modelHref = el.href
+                let resourceNameNormalized = resourceNameFull.replace(/[^a-zA-Z0-9]/g, '')
+                const modelVersionId = modelHref.split("modelVersionId=")[1]
+                const resourceTypeMap = {
+                    embedding: ".pt",
+                    lycoris: ".safetensors",
+                    lora: ".safetensors",
+                    checkpoint: ".safetensors"
+                };
+                resourceNameNormalized = resourceNameNormalized + resourceTypeMap[resourceType.toLowerCase()]
+                let fileName = resourceType.toLowerCase() + "_" + resourceNameNormalized + resourceTypeMap[resourceType.toLowerCase()];
+                let resourceDownUrl = "https://civitai.com/api/download/models/" + modelVersionId;
+                console.log(fileName)
+                // downloadURI(resourceDownUrl, fileName);
+
+                postApi(resourceDownUrl, modelVersionId, resourceType.toLowerCase(), fileName, resourceNameNormalized);
+
+
+                console.log(el);
+                console.log(resourceType);
+                console.log(resourceNameFull);
+                console.log(resourceNameNormalized);
+                console.log(modelHref)
+                console.log(modelVersionId)
+                console.log(fileName)
+            }
+        } catch (error) {
+            console.log(`ERROR: ${error}`);
+            icon.setAttribute("d", errorIconPath);
+            runButton.style.backgroundColor = "#C92A2A";
+            alert("Error. Check console for details.");
+            return;
+        }
+        icon.setAttribute("d", successIconPath);
+        runButton.style.backgroundColor = "#2F9E44";
+    });
+}
+
+// Replace the run button when the page loads
+
+replaceRunButton();
+
+// use a mutation observer to replace the run button when the page title changes
+const titleObserver = new MutationObserver(replaceRunButton);
+titleObserver.observe(document.querySelector('title'), {childList: true, subtree: true});
